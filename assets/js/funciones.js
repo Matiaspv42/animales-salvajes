12811f1f54ffcd77a92871d79efdf94a15ed4c9e
Matias <mpintov42@gmail.com>
2022-01-19 21:47:34 -0300

    Codigo funcionando, no hay errores

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,10 +1,95 @@
-c = 3
-d = 4
+import { Oso, Lobo, Leon, Serpiente, Aguila } from './tipos-animales.js'
 
-var iife = ((a = 1,b = 3)=>{
-    console.log(a,b);
-})(c,d)
+// Esta funcion instancia el animal dependiendo del tipo
+const instanciarAnimal =(tipoAnimal,edadAnimal,img,comentarios, sonido) =>{
+    // Switch para instanciar el animal que corresponda (ahora si que no se me va a olvidar hacerlo ¯\_(ツ)_/¯)
+    switch(tipoAnimal){
+        case 'Leon':
+            const leon = new Leon(tipoAnimal,edadAnimal,img,comentarios, sonido)
+            return leon
+        case 'Lobo':
+            const lobo = new Lobo(tipoAnimal,edadAnimal,img,comentarios,sonido)
+            return lobo   
+        case 'Oso':
+            const oso = new Oso(tipoAnimal,edadAnimal,img,comentarios,sonido)
+            return oso
+        case 'Serpiente':
+            const serpiente = new Serpiente(tipoAnimal,edadAnimal,img,comentarios,sonido)
+            return serpiente 
+        case 'Aguila':
+            const aguila = new Aguila(tipoAnimal,edadAnimal,img,comentarios,sonido)
+            return aguila                            
+    }
+}
 
+// Esta funcion se encarga de registrar al animal
+const registrarAnimal = (animal, animalesPlantilla,animales) => {
+    animales.push(animal) 
+    mostrarAnimales(animales, animalesPlantilla)
+}
 
+// Esta funcion muestra los animales en la seccion "Animales en Investigacion"
+// Recibe como argumento un arreglo animales con todos los animales y la plantilla donde crear los animales
+const mostrarAnimales = (animales, animalesPlantilla) => {
+    console.log(animales);
+    // Recorre el arreglo animales, primero limpia el html y luego escribe 
+    // Agrega la informacion necesaria a la img del animal en forma de data
+    // y agrega el sonido como data en el boton del sonido para poder accederlos mas tarde
+    animales.forEach(() => {
+        animalesPlantilla.innerHTML = ''
+        animales.forEach( (animal)=>{
+            animalesPlantilla.innerHTML += `
+            <div class="col-4">
+            <div class="card" style="">
+            <img src="${animal.Img()}" class="card-img-top" a
+                data-comentarios = "${animal.Comentarios()}"
+                data-edad = "${animal.Edad()}"
+                data-toggle="modal" 
+                data-target="#exampleModal"
+            >
+            <div class="bg-secondary" id="botonSonido">
+            <img src="./assets/imgs/audio.svg" style="width:17.5% ; padding:5px ; margin:0"
+            data-sonido= "${animal.Sonido()}"
+            >
+            </div>
+            </div>
+            </div>
+            `
+        })
+    })
+    let imagenesAnimales = document.querySelectorAll('#Animales img')
+    const modal = document.querySelector('.modal-body')
 
-iife
+    imagenesAnimales.forEach((imagenAnimal) => {
+        // Se agrega el eventListener que escucha el click en la imagen del animal y muestra los datos correspondientes que se encuentran almacenados como data en la misma imagen
+        imagenAnimal.addEventListener('click',()=>{
+            modal.innerHTML= `
+            <img src="${imagenAnimal.src}" class="card-img-top mb-3">
+            <h5 class= "text-center text-light">${imagenAnimal.dataset.edad}</h5>
+            <h5 class= "text-center text-light">Comentarios</h5>
+            <p class= "text-center text-light">${imagenAnimal.dataset.comentarios}</p>`
+        })
+    })
+    const sonidoAnimales = document.querySelectorAll('#botonSonido img')
+    sonidoAnimales.forEach((sonidoAnimal) => {
+        sonidoAnimal.addEventListener('click',()=>{
+            // Creo el audio inicializando la clase Audio y paso como parametro la url del boton. 
+            const audio = new Audio(`${sonidoAnimal.dataset.sonido}`)
+            audio.play()
+        })
+        
+    })
+}
+
+// Esta funcion limpia los datos del formulario
+const limpiarDatosFormulario = () => {
+    // Obtemeos los elementos del formulario y seteamos sus valores a los iniciales
+    const formDropdown = document.querySelectorAll('.form-group select')
+    formDropdown.forEach((dropdown)=> dropdown.selectedIndex = '0' )
+    const formText= document.querySelector('.form-group textarea')
+    formText.value = '' 
+    const formImage = document.getElementById('preview')
+    formImage.style.backgroundImage = 'url(http://127.0.0.1:5500/assets//imgs/lion.svg)'
+}
+
+export {instanciarAnimal, registrarAnimal,limpiarDatosFormulario}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,9 @@
-import {Animal} from './animal.js'
-import { Oso, Lobo, Leon, Serpiente, Aguila } from './tipos-animales.js'
+import {instanciarAnimal, registrarAnimal,limpiarDatosFormulario} from './funciones.js'
 
+// Comentario general: Estuve probando generar un codigo con más funciones para poder modularizar el codigo de mejor manera.
+// "La primera regla de las funciones es que deben ser pequeñas. La segunda regla de las funciones es que deben ser más pequeñas que eso."
 
-// Esta funcion instancia el animal dependiendo del tipo
-const instanciarAnimal =(tipoAnimal,edadAnimal,img,comentarios, sonido) =>{
-    // Switch para instanciar el animal que corresponda (ahora si que no se me va a olvidar hacerlo ¯\_(ツ)_/¯)
-    switch(tipoAnimal){
-        case 'Leon':
-            const leon = new Leon(tipoAnimal,edadAnimal,img,comentarios, sonido)
-            return leon
-        case 'Lobo':
-            const lobo = new Lobo(tipoAnimal,edadAnimal,img,comentarios,sonido)
-            return lobo   
-        case 'Oso':
-            const oso = new Oso(tipoAnimal,edadAnimal,img,comentarios,sonido)
-            return oso
-        case 'Serpiente':
-            const serpiente = new Serpiente(tipoAnimal,edadAnimal,img,comentarios,sonido)
-            return serpiente 
-        case 'Aguila':
-            const aguila = new Aguila(tipoAnimal,edadAnimal,img,comentarios,sonido)
-            return aguila                            
-    }
-}
+//Para activar el sonido de los animales en animales de investigacion hay que hacer click en el icono de sonido. 
 
 // Aqui esta la IIFE que va a pedir el json
 // Es una consulta asincronica que obtiene las imagenes y todos los datos de los animales
@@ -44,85 +25,19 @@ const obtenerData = async(tipoAnimal)=>{
     return variable
 }
 
-const modal = document.querySelector('.modal-body')
 
-const registrarAnimal = (animal) => {
-    animales.push(animal) 
-    mostrarAnimales(animales, animalesPlantilla)
-}
+// Elementos del html
 
-
-// Esta funcion muestra los animales en la seccion "Animales en Investigacion"
-// Recibe como argumento un arreglo animales con todos los animales y la plantilla donde crear los animales
-const mostrarAnimales = (animales, animalesPlantilla) => {
-    // Recorre el arreglo animales y primero limpia el html y luego escribe 
-    animales.forEach(() => {
-        animalesPlantilla.innerHTML = ''
-        animales.forEach( (animal)=>{
-            animalesPlantilla.innerHTML += `
-            <div class="col-4">
-            <div class="card" style="">
-            <img src="${animal.Img()}" class="card-img-top" a
-                data-comentarios = "${animal.Comentarios()}"
-                data-edad = "${animal.Edad()}"
-                data-toggle="modal" 
-                data-target="#exampleModal"
-            >
-            <div class="bg-secondary" id="botonSonido">
-            <img src="./assets/imgs/audio.svg" style="width:17.5% ; padding:5px ; margin:0"
-            data-sonido= "${animal.Sonido()}"
-            >
-            </div>
-            </div>
-            </div>
-            `
-        })
-    })
-    
-    imagenesAnimales = document.querySelectorAll('#Animales img')
-    imagenesAnimales.forEach((imagenAnimal) => {
-        imagenAnimal.addEventListener('click',()=>{
-            modal.innerHTML= `
-            <img src="${imagenAnimal.src}" class="card-img-top mb-3">
-            <h5 class= "text-center text-light">${imagenAnimal.dataset.edad}</h5>
-            <h5 class= "text-center text-light">Comentarios</h5>
-            <p class= "text-center text-light">${imagenAnimal.dataset.comentarios}</p>`
-        })
-    })
-    const sonidoAnimales = document.querySelectorAll('#botonSonido img')
-    sonidoAnimales.forEach((sonidoAnimal) => {
-        console.log(sonidoAnimal.dataset.sonido);
-        sonidoAnimal.addEventListener('click',()=>{
-            const audio = new Audio(`${sonidoAnimal.dataset.sonido}`)
-            console.log(audio);
-            audio.play()
-        })
-        
-    })
-}
-
-// Esta funcion limpia los datos del formulario
-const limpiarDatosFormulario = () => {
-    // Obtemeos los elementos del formulario y seteamos sus valores a los iniciales
-    const formDropdown = document.querySelectorAll('.form-group select')
-    formDropdown.forEach((dropdown)=> dropdown.selectedIndex = '0' )
-    const formText= document.querySelector('.form-group textarea')
-    formText.value = '' 
-    const formImage = document.getElementById('preview')
-    formImage.style.backgroundImage = 'url(http://127.0.0.1:5500/assets//imgs/lion.svg)'
-}
-let animales = []
 let preview = document.querySelector('#preview')
 let animalesPlantilla = document.getElementById('Animales')
+const ruedaAnimal = document.getElementById('animal')
+const boton = document.getElementById('btnRegistrar')
 
-let imagenesAnimales = null
-
+// Variables que son usadas en los event listener
 let data = null
 let fuenteImagen = null
+var animales = []
 
-const ruedaAnimal = document.getElementById('animal')
-
-const boton = document.getElementById('btnRegistrar')
 
 // Se agrega el EventListener para escuchar cuando se haga click en boton Agregar
 boton.addEventListener('click', async (e)=>{
@@ -139,7 +54,7 @@ boton.addEventListener('click', async (e)=>{
     const urlImagen = fuenteImagen.slice(5,-2)
     const urlSonido = `http://127.0.0.1:5500/assets/sounds/${sonidoAnimal}`
     const animal = instanciarAnimal(tipoAnimal, edadAnimal, urlImagen, comentarios, urlSonido)
-    registrarAnimal(animal)
+    registrarAnimal(animal, animalesPlantilla, animales)
     limpiarDatosFormulario()
     }
     //En caso que falte uno manda una alerta
@@ -148,9 +63,7 @@ boton.addEventListener('click', async (e)=>{
     }
 })
 
-
-
-// Se agrega el EventListener para escuchar cuando se cambie los cambios de la rueda y así ir poniendo las imagenes del animal que corresponde
+// Se agrega el EventListener para escuchar cuando se selccione algo en la rueda y asi ir poniendo las imagenes del animal que corresponde
 ruedaAnimal.addEventListener('change', async ()=>{
     const tipoAnimal = document.getElementById('animal').value
     data =  await obtenerData(tipoAnimal)
@@ -160,9 +73,8 @@ ruedaAnimal.addEventListener('change', async ()=>{
     fuenteImagen = await preview.style.backgroundImage
 })
 
-
-
-// Esta linea está solo para verificar que el metodo Rugir y los demás funcionan
+// Esta linea está solo para verificar que el metodo Rugir y los demás funcionan.
+//  Si uno lo descomenta, al abrirse la pagina el leon ruge así que funciona bien
 
 // const leon = new Leon('leon','12','ddd','comentario','http://127.0.0.1:5500/assets/sounds/Rugido.mp3')
 // leon.Rugir()
